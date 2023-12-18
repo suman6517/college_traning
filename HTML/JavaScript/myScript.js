b29b1a43dca8cc3c8fe7bb793bbad5863bae4303
Suman Baral <ssumanbaral55@gmail.com>
2023-12-18 12:55:08 +0530

    Java Script

--- HTML/JavaScript/myScript.js
+++ HTML/JavaScript/myScript.js
@@ -1,22 +1,20 @@
-// let details = {
-//     name : function(name)
-//     {
-//         console.log("Your name is " +name);
-//     },
+let details = {
+    name : function(name)
+    {
+        console.log("Your name is " +name);
+    },
 
-//     phone: function(phoneNumber)
-//     {
-//             console.log("your Phone Number is "+phoneNumber);
-//     },
+    phone: function(phoneNumber)
+    {
+            console.log("your Phone Number is "+phoneNumber);
+    },
 
-//     address: function(localArea)
-//     {
-//             console.log("your Local Area is "+localArea);
-//     }
-// }
+    address: function(localArea)
+    {
+            console.log("your Local Area is "+localArea);
+    }
+}
 
-// details.name("Suman");
-// details.phone(5595196);
-// details.address("Kandi")
-
-let text = 
+details.name("Suman");
+details.phone(5595196);
+details.address("Kandi")
