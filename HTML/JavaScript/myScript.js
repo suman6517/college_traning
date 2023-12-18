5e6a675cbfa970eaf8813052358170d7a2146cf0
Suman Baral <ssumanbaral55@gmail.com>
2023-12-18 12:36:11 +0530

    till 18

---
+++ HTML/JavaScript/myScript.js
@@ -0,0 +1,22 @@
+// let details = {
+//     name : function(name)
+//     {
+//         console.log("Your name is " +name);
+//     },
+
+//     phone: function(phoneNumber)
+//     {
+//             console.log("your Phone Number is "+phoneNumber);
+//     },
+
+//     address: function(localArea)
+//     {
+//             console.log("your Local Area is "+localArea);
+//     }
+// }
+
+// details.name("Suman");
+// details.phone(5595196);
+// details.address("Kandi")
+
+let text = 
